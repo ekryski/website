1fa1249cf7242a262d2fe9a884d55f7448e58f05
Eric Kryski <e.kryski@gmail.com>
2025-02-26 16:40:44 -0700

    replacing template with tailwind UI template

--- data/headerNavLinks.js
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/about', title: 'About' },
-  { href: '/speaking', title: 'Speaking' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/insights', title: 'Insights' },
-  { href: '/affiliations', title: 'Affiliations' },
-]
-
-export default headerNavLinks
--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,7 @@
+/** @type {import('prettier').Options} */
 module.exports = {
-  semi: false,
   singleQuote: true,
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: 'es5',
-  bracketSpacing: true,
+  semi: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/styles/tailwind.css',
 }
